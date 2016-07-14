28dfade5eb0297bfc6f552981afa1bcd389ebf6f
ipoplar <ipoplarwhite@gmail.com>
2016-07-14 12:09:16 +0800

    pos v0

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,7 +1,128 @@
-require('../src/main.js');
+describe('intergation test', function() {
+  var allItems;
+  var inputs;
 
-describe("A suite", function() {
-  it("contains spec with an expectation", function() {
-    expect(add(1, 2)).toBe(3);
+  beforeEach(function() {
+    allItems = loadAllItems();
+    inputs = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2',
+      'ITEM000005',
+      'ITEM000005',
+      'ITEM000005'
+    ];
+});
+    it('should print correct text', function() {
+
+      spyOn(console, 'log');
+
+      printReceipt(inputs);
+
+      var expectText =
+        '***<没钱赚商店>收据***\n' +
+        '名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)\n' +
+        '名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)\n' +
+        '名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)\n' +
+        '----------------------\n' +
+        '总计：51.00(元)\n' +
+        '节省：7.50(元)\n' +
+        '**********************';
+
+      expect(console.log).toHaveBeenCalledWith(expectText);
+    });
+});
+describe('unit test',function () {
+  beforeEach(function() {
+    allItems = loadAllItems();
+    inputs = [
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000001',
+      'ITEM000003-2',
+      'ITEM000005',
+      'ITEM000005',
+      'ITEM000005'
+    ];
+  });
+  describe('buildCartItems', function () {
+    it('should print cartItems', function () {
+      var cartItems = buildCartItems(inputs);
+      var expectCartItems = [
+        {
+          input: {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00},
+          count: 5
+        },
+        {
+          input: {barcode: 'ITEM000003-2', name: '荔枝', unit: '斤', price: 15.00},
+          count: 2
+        },
+        {
+          input: {barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.50},
+          count: 3
+        }
+      ];
+      expect(cartItems).toEqual(expectCartItems);
+
+    });
+  });
+
+  describe('buildReceiptItems', function () {
+    it('should print receiptItems', function () {
+      var receiptItems = buildReceiptItems(buildCartItems(inputs));
+      var expectReceiptItems = [
+        {
+          cartItem: {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00, count: 5},
+          subtotal: 12.00,
+          saved: 3.00
+        },
+        {
+          cartItem: {barcode: 'ITEM000003-2', name: '荔枝', unit: '斤', price: 15.00, count: 2},
+          subtotal: 30.00,
+          saved: 0.00
+        },
+        {
+          cartItem: {barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.50, count: 3},
+          subtotal: 9.00,
+          saved: 4.50
+        }
+      ];
+      expect(receiptItems).toEqual(expectReceiptItems);
+    });
+  });
+
+  describe('buildReceipt', function () {
+    it('should print receipt', function () {
+      var receipt = buildReceipt(buildReceiptItems(buildCartItems(inputs)));
+      var expectReceipt = {
+        receiptItems: [
+          {
+            cartItem: {barcode: 'ITEM000001', name: '雪碧', unit: '瓶', price: 3.00, count: 5},
+            subtotal: 12.00,
+            saved: 3.00
+          },
+          {
+            cartItem: {barcode: 'ITEM000003-2', name: '荔枝', unit: '斤', price: 15.00, count: 2},
+            subtotal: 30.00,
+            saved: 0.00
+          },
+          {
+            cartItem: {barcode: 'ITEM000005', name: '方便面', unit: '袋', price: 4.50, count: 3},
+            subtotal: 9.00,
+            saved: 4.5
+          }
+        ],
+        total: 51.00,
+        discount: 7.50
+      };
+
+      expect(receipt).toEqual(expectReceipt);
+
+    });
   });
 });
--- src/main.js
+++ src/main.js
@@ -1,1 +1,101 @@
-add = function(a, b) { return a + b; }
+function printReceipt(inputs) {
+  var cartItems = buildCartItems(inputs);
+  var receiptItems = buildReceiptItems(cartItems);
+  var receipt = buildReceipt(receiptItems);
+
+  var cartItemText = '';
+  var expectText = '';
+
+  for(var i = 0; i < receipt.receiptItems.length; i++){
+    cartItemText += '名称：' + receipt.receiptItems[i].cartItem.name + '，' + '数量：'
+      + receipt.receiptItems[i].cartItem.count + receipt.receiptItems[i].cartItem.unit
+      + '，' + '单价：' + receipt.receiptItems[i].cartItem.price.toFixed(2) + '(元)' + '，'
+      + '小计：' + receipt.receiptItems[i].subtotal.toFixed(2) + '(元)\n' ;
+  }
+  expectText += '***<没钱赚商店>收据***\n' + cartItemText +'----------------------\n' +
+    '总计：'+ receipt.total.toFixed(2) +'(元)\n' +
+    '节省：'+ receipt.discount.toFixed(2) +'(元)\n'+
+    '**********************';
+  console.log(expectText);
+}
+
+function buildReceipt(receiptItems) {
+  var receipt = {};
+  var total = 0;
+  var discount = 0;
+  for(var i = 0; i < receiptItems.length; i++){
+    total += receiptItems[i].subtotal
+    discount += receiptItems[i].saved;
+  }
+  receipt = {receiptItems: receiptItems, total:total,discount:discount};
+  return receipt;
+}
+function isPromotion(cartItem) {
+  var promotions = loadPromotions();
+  var existLine = cartItem.input.barcode.split("-");
+  var isPromot = false;
+  for(var i = 0; i < promotions[0].barcodes.length; i++){
+    if(existLine[0] === promotions[0].barcodes[i]){
+      isPromot = true;
+      return isPromot;
+    }else{
+      isPromot = false;
+    }
+  }
+  return isPromot;
+}
+function buildReceiptItems(cartItems) {
+  var receiptItems = [];
+  for(var i = 0; i < cartItems.length; i++){
+    if(isPromotion(cartItems[i]) && cartItems[i].count >= 3){
+      subtotal = (cartItems[i].count-parseInt(cartItems[i].count/3))*cartItems[i].input.price;
+    }else{
+      subtotal = cartItems[i].count*cartItems[i].input.price;
+    }
+    saved = cartItems[i].count*cartItems[i].input.price-subtotal;
+    receiptItems.push({cartItem:{barcode:cartItems[i].input.barcode,name:cartItems[i].input.name,
+      unit:cartItems[i].input.unit, price:cartItems[i].input.price,count:cartItems[i].count},
+      subtotal:subtotal,saved:saved});
+  }
+  return receiptItems;
+}
+function buildCartItems(inputs) {
+  var items = buildItems(inputs);
+  var cartItems = [];
+  items.forEach(function (item) {
+    cartItems = isExist(item, cartItems);
+  });
+  return cartItems;
+}
+
+function buildItems(inputs) {
+  var items = [];
+  var allItems = loadAllItems();
+
+  inputs.forEach(function (input) {
+    var existLine = input.split("-");
+    for(var i = 0; i < allItems.length; i++){
+      if(existLine[0] === allItems[i].barcode){
+        items.push({barcode:input,name:allItems[i].name,
+          unit:allItems[i].unit,price:allItems[i].price});
+      }
+    }
+  });
+  return items;
+}
+function isExist(item, cartItems){
+  var existLine = item.barcode.split("-");
+  for(var i = 0; i < cartItems.length; i++){
+    if(existLine[1]){
+      cartItems[cartItems.length] = {input:item,count:parseInt(existLine[1])};
+      return cartItems;
+    }else{
+      if(existLine[0] == cartItems[i].input.barcode){
+        cartItems[i].count++;
+        return cartItems;
+      }
+    }
+  }
+  cartItems[cartItems.length] = {input:item,count:1};
+  return cartItems;
+}
